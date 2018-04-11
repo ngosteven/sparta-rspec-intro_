7c09e03ac28ab8af1bdb6eabfc8dac5ede695a62
Tech-A47 <tech-a47@Tech-A47s-MacBook-Air.local>
2018-04-11 13:54:41 +0100

    Fibonnaci

--- rspec_project_euler/problem2/lib/fibonacci.rb
+++ rspec_project_euler/problem2/lib/fibonacci.rb
@@ -1,6 +1,28 @@
-sum, x, y = 0, 1, 2
-	while y < 4000000
-	  sum += y if y.even?
-	  x, y = y, x + y
-	end
-	puts sum
+class ProjectFib
+
+attr_accessor :projectfib_array, :evens, :total
+
+  def initialize
+    @projectfib_array = [1, 2]
+    @evens = []
+    @total = 0
+  end
+
+  def fib
+    while @projectfib_array[-1] + @projectfib_array[-2] < 4000000 do
+      @projectfib_array << @projectfib_array[-1] + @projectfib_array[-2]
+    end
+  end
+
+  def list_evens
+    @projectfib_array.each do |i|
+      if i.even? then @evens << i
+      end
+    end
+  end
+
+  def total
+    @evens.sum
+  end
+
+end
--- rspec_project_euler/problem2/spec/fibonacci_spec.rb
+++ rspec_project_euler/problem2/spec/fibonacci_spec.rb
@@ -1,5 +1,15 @@
-require 'fibonacci'
+require 'fibonnaci'
 
-describe 'Fibonacci' do
-	
+describe 'ProjectFib' do
+
+  before(:each) do
+    @fi = ProjectFib.new
+  end
+
+  it 'should return false if the list exceeds 4000000' do
+    @fi.projectfib_array.each do |i|
+      expect(i).to be < 4000000
+    end
+  end
+
 end
